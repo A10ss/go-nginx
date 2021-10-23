ad726f4f4fa0ea81b78f0ce36785f8afa7a12d01
LinusLiu <la10ss@163.com>
2021-10-23 15:23:37 +0800

    #commit 清除其他文件，修改路径配置

--- main.go
+++ main.go
@@ -1,39 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 var (
 	err  error
-	path = "D:/GoWorkSpace/go-nginx/WebApp/demo-app"
+	path = "WebApp/my-app"
 )
 
-func FilePathScanner(localpath string, routeMap map[string]string) {
-	dir, err := ioutil.ReadDir(localpath)
-	if err != nil {
-		fmt.Println(err)
-	}
-	for _, ele := range dir {
-		if ele.IsDir() {
-			fmt.Println(localpath + "/" + ele.Name())
-			FilePathScanner(localpath+"/"+ele.Name(), routeMap)
-		} else {
-			routepath := []rune(localpath)[len([]rune(path)):]
-			routeMap[localpath+"/"+ele.Name()] = string(routepath) + "/" + ele.Name()
-		}
-	}
-}
-
 func main() {
-	routeMap := make(map[string]string)
-	FilePathScanner(path, routeMap)
-
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("D:\\GoWorkSpace\\go-nginx\\WebApp\\my-app")))
+	mux.Handle("/", http.FileServer(http.Dir(path)))
 
 	server := &http.Server{
 		Addr:    ":8080",
